f69532b4ba0e1928fc09a34e1308f8b7a9dd4ced
shashwat172002 <shashwatmani17@gmail.com>
2024-09-15 15:12:10 +0530

    fc-12

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,12 +4,12 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy: {
-      '/api': {
-        target: 'https://drop-dorm.vercel.app/',       //https://dormdrop.onrender.com   //http://localhost:3000
-        secure: false,
-      },
-    },
+    // proxy: {
+    //   '/api': {
+    //     target: 'https://drop-dorm.vercel.app/',       //https://dormdrop.onrender.com   //http://localhost:3000
+    //     secure: false,
+    //   },
+    // },
   },
   plugins: [react()],
 });
