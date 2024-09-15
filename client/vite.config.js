e3fbb99c50faaf2dc57ba7229baabe4b83c53777
shashwat172002 <shashwatmani17@gmail.com>
2024-09-15 13:50:52 +0530

    fc-2

--- client/vite.config.js
+++ client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',       //https://dormdrop.onrender.com   //http://localhost:3000
+        target: 'https://drop-dorm.vercel.app/',       //https://dormdrop.onrender.com   //http://localhost:3000
         secure: false,
       },
     },
